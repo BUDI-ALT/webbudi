c0ef42f2c12bd8bee447c47a07d6b5c465e100c6
BUDI-ALT <uwoybudi4@gmail.com>
2024-06-17 00:05:38 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Bucin</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>CIPA</h1>
+    </header>
+    <section id="gallery">
+        <h2>Galeri Foto</h2>
+        <div class="photo-container">
+            <img src="images/foto.jpg.jpeng" alt="Foto Pacar 1">
+            <img src="images/foto1.jpg,jeng" alt="Foto Pacar 2">
+        </div>
+    </section>
+    <section id="quotes">
+        <h2>buat mu</h2>
+        <div class="quotes-container">
+            <p>"Kamu adalah segalanya bagiku."</p>
+            <p>"Cinta kita akan abadi selamanya."</p>
+        </div>
+    </section>
+    <div class="hearts"></div>
+    <footer>
+        <p>&copy; ujicoba</p>
+    </footer>
+    <script src="script.js"></script>
+</body>
+</html>
